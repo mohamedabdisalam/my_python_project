3ccdeb3e7e8040bec896d18ae5983afabd028bc5
Liam Hackett <liamjhackett1@gmail.com>
2023-11-15 15:39:31 +0000

    added completed tests for check grammar.

--- lib/check_grammar.py
+++ lib/check_grammar.py
@@ -2,5 +2,7 @@ def check_grammar(text):
     if text[0].isupper():
         if text[-1] in '!.?':
             return True
+        else:
+            return False 
     else:
         return False
--- tests/test_check_grammar.py
+++ tests/test_check_grammar.py
@@ -3,3 +3,12 @@ from lib.check_grammar import *
 def test_capital_letter_and_punctuation_mark():
     result = check_grammar('This is a sentence.')
     assert result == True
+
+
+def test_capital_letter_and_incorrect_punctuation_mark():
+    result = check_grammar('This is a sentence,')
+    assert result == False
+
+def test_lower_case_letter_and_punctuation_mark():
+    result = check_grammar('this is a sentence.')
+    assert result == False
