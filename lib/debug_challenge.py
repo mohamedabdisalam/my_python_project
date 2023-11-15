e339d76453b95924fe5ac4a2d27faa54fca17b96
mohamed <mohamed.abdisalam@gmail.com>
2023-11-15 16:48:15 +0000

    Working through the example

--- lib/debug_challenge.py
+++ lib/debug_challenge.py
@@ -1,9 +1,12 @@
 def get_most_common_letter(text):
     counter = {}
-    print(f"counter is ")
+    print(f"at the start counter is {counter}")
     for char in text:
-        counter[char] = counter.get(char, 0) + 1
-    letter = sorted(counter.items(), key=lambda item: item[1])[0][1]
+        if char.isalpha():
+            counter[char] = counter.get(char, 0) + 1
+    letter = sorted(counter.items(), key=lambda item: item[1])[0][0]
+    print(f"letter is now {letter}")
+    print(f"now counter is {counter}")
     return letter
 
 
