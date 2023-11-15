9206649f3e3f14a07c363518e115126b038d2d0e
mohamed <mohamed.abdisalam@gmail.com>
2023-11-15 14:32:32 +0000

    Reading time function done with passing test

--- lib/reading_time.py
+++ lib/reading_time.py
@@ -1,2 +1,4 @@
 def estimate_reading_time(text):
-    
+    word_count = len(text.split())
+    estimate = int(word_count / 200)
+    return f"{estimate} minute"
--- tests/test_reading_time.py
+++ tests/test_reading_time.py
@@ -12,17 +12,17 @@ def test_time_to_read_200_words():
     result = estimate_reading_time(input_text)
     assert result == "1 minute"
 
-"""
-Given 400 words 
-It returns 2 minute 
-"""
+# """
+# Given 400 words 
+# It returns 2 minute 
+# """
 
-estimate_reading_time(text) => 2 min
+# estimate_reading_time(text) => 2 min
 
-"""
-Given 0 words 
-It returns 0 minutes
-"""
+# """
+# Given 0 words 
+# It returns 0 minutes
+# """
 
-estimate_reading_time(text) => 2 min
-def test
+# estimate_reading_time(text) => 2 min
+# def test
