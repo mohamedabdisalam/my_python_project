f0f58b76246baaa73499c248708827101c70e2c7
mohamed <mohamed.abdisalam@gmail.com>
2023-11-15 14:37:33 +0000

    All tests for reading time DONE

--- tests/test_reading_time.py
+++ tests/test_reading_time.py
@@ -12,17 +12,32 @@ def test_time_to_read_200_words():
     result = estimate_reading_time(input_text)
     assert result == "1 minute"
 
-# """
-# Given 400 words 
-# It returns 2 minute 
-# """
+"""
+Given 400 words 
+It returns 2 minute 
+"""
+
+def test_time_to_read_400_words():
+    input_text = []
+    for i in range(400):
+        input_text.append("word")
+    input_text = " ".join(input_text)
+    result = estimate_reading_time(input_text)
+    assert result == "2 minute"
 
 # estimate_reading_time(text) => 2 min
 
-# """
-# Given 0 words 
-# It returns 0 minutes
-# """
+"""
+Given 0 words 
+It returns 0 minutes
+"""
+
+def test_time_to_read_0_words():
+    input_text = []
+    result = estimate_reading_time("")
+    assert result == "0 minute"
+
+    
 
 # estimate_reading_time(text) => 2 min
 # def test
